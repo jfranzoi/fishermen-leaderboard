f70d59394a721131331a3e290cae296103c42ba3
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2023-06-11 01:09:42 +0200

    UI: minimal setup, ligther

--- fishermen-leaderboard-ui/app/components/FishermanCard.tsx
+++ fishermen-leaderboard-ui/app/components/FishermanCard.tsx
@@ -1,11 +1,13 @@
 import { Fisherman } from "../hooks/useFishermen";
+import styles from '../page.module.css';
+
 declare type Props = {
     content: Fisherman;
 };
 
 export default function FishermanCard({ content }: Props): JSX.Element {
     return (
-        <div>
+        <div className={styles.card}>
             <span>{content.name}: {content.amount} kg</span>
         </div>
     );
--- fishermen-leaderboard-ui/app/page.tsx
+++ fishermen-leaderboard-ui/app/page.tsx
@@ -2,18 +2,18 @@
 
 import { useFishermen } from './hooks/useFishermen';
 import FishermanCard from './components/FishermanCard';
+import styles from './page.module.css';
 
 export default function Home() {
 
   const [fishermen, isLoading, loadNext] = useFishermen();
 
   if (isLoading) return <p>Loading...</p>
-  if (!fishermen) return <p>No results</p>
 
   return (
     <main>
-      <div>
-        <h1>Leaderboard</h1>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Top Scorer</h1>
         {fishermen.map((each) => {
           return (
             <FishermanCard content={each} />
