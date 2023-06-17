3527259810012e576634741826d6295800fa8454
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2023-06-17 11:43:20 +0200

    deployment: added proxy, on api only WIP

--- build.sh
+++ build.sh
@@ -2,11 +2,25 @@
 
 echo "Building"
 docker-compose build
+
+docker tag fishermen-leaderboard-proxy:latest ghcr.io/jfranzoi/fishermen-leaderboard-proxy:latest
+docker tag fishermen-leaderboard-proxy:latest ghcr.io/jfranzoi/fishermen-leaderboard-proxy:00-$( git rev-parse --short HEAD )
+
+docker tag fishermen-leaderboard-api:latest ghcr.io/jfranzoi/fishermen-leaderboard-api:latest
 docker tag fishermen-leaderboard-api:latest ghcr.io/jfranzoi/fishermen-leaderboard-api:00-$( git rev-parse --short HEAD )
+
+docker tag fishermen-leaderboard-ui:latest ghcr.io/jfranzoi/fishermen-leaderboard-ui:latest
 docker tag fishermen-leaderboard-ui:latest ghcr.io/jfranzoi/fishermen-leaderboard-ui:00-$( git rev-parse --short HEAD )
 
 echo "Publishing"
+
+docker push ghcr.io/jfranzoi/fishermen-leaderboard-proxy:latest
+docker push ghcr.io/jfranzoi/fishermen-leaderboard-proxy:00-$( git rev-parse --short HEAD )
+
+docker push ghcr.io/jfranzoi/fishermen-leaderboard-api:latest
 docker push ghcr.io/jfranzoi/fishermen-leaderboard-api:00-$( git rev-parse --short HEAD )
+
+docker push ghcr.io/jfranzoi/fishermen-leaderboard-ui:latest
 docker push ghcr.io/jfranzoi/fishermen-leaderboard-ui:00-$( git rev-parse --short HEAD )
 
 echo "Done"
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-PATH="$PATH:~/workspace/yoke" yoke install -c public-ecs01 -s fishermen-leaderboard -t 00-$( git rev-parse --short HEAD )
+PATH="$PATH:~/workspace/yoke" yoke install -c public-ecs01 -s fishermen-leaderboard -t latest
